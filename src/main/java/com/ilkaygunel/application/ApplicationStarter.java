2303d1acb3d25fe53dc8021f38624146710a8af5
İlkay Günel <turgi@Turgi-MBP.home>
2023-07-09 16:07:14 +0300

    Adding Swagger Support For Documentation

--- src/main/java/com/ilkaygunel/application/ApplicationStarter.java
+++ src/main/java/com/ilkaygunel/application/ApplicationStarter.java
@@ -3,9 +3,11 @@ package com.ilkaygunel.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ilkaygunel.*"})
+@EnableWebMvc
 public class ApplicationStarter {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationStarter.class);

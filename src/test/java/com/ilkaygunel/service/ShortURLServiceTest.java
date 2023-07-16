f3dbd402dd09cdbc67a1ed54f24e3d4f60ceb9b6
İlkay Günel <turgi@192.168.1.102>
2023-07-16 23:57:17 +0300

    Refactoring on test code.

--- src/test/java/com/ilkaygunel/service/ShortURLServiceTest.java
+++ src/test/java/com/ilkaygunel/service/ShortURLServiceTest.java
@@ -1,27 +1,23 @@
 package com.ilkaygunel.service;
 
-import com.ilkaygunel.configuration.GeneralConfig;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-//@TestPropertySource(locations = "classpath:application-test.properties")
-@ContextConfiguration(classes = GeneralConfig.class)
-//@ActiveProfiles("test")
-//@AutoConfigureMockMvc
+@TestPropertySource(locations = "classpath:application-test.properties")
+@ContextConfiguration(classes = ShortURLService.class)
 public class ShortURLServiceTest {
 
-    @InjectMocks
-    ShortURLService shortURLService;
+    @Autowired
+    private ShortURLService shortURLService;
 
     @Value("${short.url.domain}")
     private String shortUrlDomain;
@@ -32,13 +28,10 @@ public class ShortURLServiceTest {
     @BeforeEach
     public void init() {
         MockitoAnnotations.openMocks(this);
-        ReflectionTestUtils.setField(shortURLService, "shortUrlDomain", shortUrlDomain);
-        ReflectionTestUtils.setField(shortURLService, "shortUrlPort", shortUrlPort);
     }
 
     @Test
     public void testShortenURL() {
-        String a = shortUrlDomain;
         String shortUrl = shortURLService.shortenURL("http://ilkaygunel.com");
         assertTrue(shortUrl.contains(shortUrlDomain.concat(":").concat(shortUrlPort)));
     }
